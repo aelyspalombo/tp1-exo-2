fe218c278d16c0593da3bd9f511a9a00fdf04850
aelyspalombo <68046095+aelyspalombo@users.noreply.github.com>
2020-07-16 14:21:04 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    led.unplot(x, y)
+    x = x - 1
+    led.plot(x, y)
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(x, y)
+    x += 1
+    led.plot(x, y)
+})
+let y = 0
+let x = 0
+x = 0
+y = 0
+led.plot(x, y)
+basic.forever(function () {
+    basic.pause(1000)
+    led.unplot(x, y)
+    y += 1
+    led.plot(x, y)
+    if (y == 4) {
+        while (y != 0) {
+            basic.pause(1000)
+            led.unplot(x, y)
+            y = y - 1
+            led.plot(x, y)
+        }
+    }
+})
